9786848c14931285acf4db44c538f462b0ce2031
Marina Sidorkina <m.sidorkina55@gmail.com>
2018-09-08 17:44:44 +0300

    Добавляет исправления в первое задание к третьей лекции: пока все дома

--- js/goods.js
+++ js/goods.js
@@ -1,4 +1,36 @@
 'use strict';
+// ПЕРВОЕ ЗАДАНИЕ
+var ARRAY_LENGTH = 26;
+
+var amountParams = {
+  MIN: 0,
+  MAX: 20
+};
+
+var priceParams = {
+  MIN: 100,
+  MAX: 1500
+};
+
+var weightParams = {
+  MIN: 30,
+  MAX: 300
+};
+
+var valueParams = {
+  MIN: 1,
+  MAX: 5
+};
+
+var numberParams = {
+  MIN: 10,
+  MAX: 900
+};
+
+var energyParams = {
+  MIN: 70,
+  MAX: 500
+};
 
 var nameArray = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
 
@@ -14,24 +46,40 @@ var renderObject = function (nameArr, pictureArr, contentsArr) {
   var object = {};
   object.name = nameArr[getRandom(0, nameArr.length)];
   object.picture = pictureArr[getRandom(0, pictureArr.length)];
-  object.amount = getRandom(0, 21);
-  object.price = getRandom(100, 151);
-  object.weight = getRandom(30, 301);
+  object.amount = getRandom(amountParams.MIN, amountParams.MAX + 1);
+  object.price = getRandom(priceParams.MIN, priceParams.MAX + 1);
+  object.weight = getRandom(weightParams.MIN, weightParams.MAX + 1);
   object.rating = {};
-  object.rating.value = getRandom(1, 6);
-  object.rating.number = getRandom(10, 901);
+  object.rating.value = getRandom(valueParams.MIN, valueParams.MAX + 1);
+  object.rating.number = getRandom(numberParams.MIN, numberParams.MAX + 1);
   object.nutritionFacts = {};
   object.nutritionFacts.sugar = Boolean(getRandom(0, 2));
-  object.nutritionFacts.energy = getRandom(70, 501);
+  object.nutritionFacts.energy = getRandom(energyParams.MIN, energyParams.MAX + 1);
   object.nutritionFacts.contents = contentsArr[getRandom(0, contentsArr.length)];
   return object;
 };
 
-var renderArray = function () {
+var renderArray = function (arrayLength) {
   var array = [];
-  for (var i = 0; i <= 25; i++) {
+  for (var i = 0; i < arrayLength; i++) {
     var element = renderObject(nameArray, pictureArray, contentsArray);
     array[i] = element;
   }
   return array;
+};
+
+var goodsArray = renderArray(ARRAY_LENGTH)
+
+// ВТОРОЕ ЗАДАНИЕ
+var CATALOG__CARDS = document.querySelector('.catalog__cards');
+var CATALOG__LOAD = document.querySelector('.catalog__load');
+
+CATALOG__CARDS.classList.remove('catalog__cards--load');
+CATALOG__LOAD.classList.add('visually-hidden');
+
+var CARD__TEMPLATE = document.querySelector('#card').content.querySelector('article');
+
+var renderDomObject = function (object) {
+  var card = CARD__TEMPLATE.cloneNode(true);
+
 };
