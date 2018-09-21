f361024538ab93900b43a048acad590c3d9e358f
Marina Sidorkina <m.sidorkina55@gmail.com>
2018-09-21 03:14:45 +0300

    Добавляет п.4 задания к четвертой лекции: подробности (Фильтр по цене)

--- js/goods.js
+++ js/goods.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var CATALOG_CARDS_LIST_LENGTH = 26;
+var PRICE_RANGE = 100;
 var order = [];
 var catalogCards = document.querySelector('.catalog__cards');
 var catalogLoad = document.querySelector('.catalog__load');
@@ -17,6 +18,15 @@ var deliverInStoreBtn = document.querySelector('#deliver__store');
 var deliverByCourierBtn = document.querySelector('#deliver__courier');
 var deliverInStoreBlock = document.querySelector('.deliver__store');
 var deliverByCourierBlock = document.querySelector('.deliver__courier');
+var priceRangeFilterBtnLeft = document.querySelector('.range__btn--left');
+var priceRangeFilterBtnRight = document.querySelector('.range__btn--right');
+var priceRangeMinPinValue = document.querySelector('.range__price--min');
+var priceRangeMaxPinValue = document.querySelector('.range__price--max');
+var priceRangeFilter = document.querySelector('.range__filter');
+var priceRangeFilterCoordinate = priceRangeFilter.getBoundingClientRect();
+var priceRangeStart = priceRangeFilterCoordinate.x;
+var priceRangeWidth = priceRangeFilterCoordinate.width;
+var priceRangeStep = priceRangeWidth / PRICE_RANGE;
 
 var amountParams = {
   MIN: 0,
@@ -251,6 +261,21 @@ var createOrderElements = function (array) {
   return orderFragment;
 };
 
+var checkGoodsInOrder = function (array, objectName) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].name === objectName) {
+      var index = i;
+    }
+  }
+  return index;
+};
+
+var getWordEnding = function (number, titles) {
+  var cases = [2, 0, 1, 1, 1, 2];
+  return titles[(number % 100 > 4 && number % 100 < 20) ?
+    2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+};
+
 var addGoodToCart = function (object) {
   var orderObject = Object.assign({}, object);
   var index = checkGoodsInOrder(order, object.name);
@@ -270,19 +295,11 @@ var addGoodToCart = function (object) {
   }
 };
 
-var checkGoodsInOrder = function (array, objectName) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i].name === objectName) {
-      var index = i;
-    }
-  }
-  return index;
-};
-
-var getWordEnding = function (number, titles) {
-  var cases = [2, 0, 1, 1, 1, 2];
-  return titles[(number % 100 > 4 && number % 100 < 20) ?
-    2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+var getPriceRangePinCoordinate = function (evtTarget) {
+  var pinCoordinate = evtTarget.getBoundingClientRect();
+  var pinX = pinCoordinate.x;
+  var pinValue = Math.round((pinX - priceRangeStart) / priceRangeStep);
+  return pinValue;
 };
 
 var catalogGoodsArray = renderGoodsArray(CATALOG_CARDS_LIST_LENGTH);
@@ -315,4 +332,12 @@ deliverByCourierBtn.addEventListener('click', function () {
     deliverByCourierBlock.classList.remove('visually-hidden');
     deliverInStoreBlock.classList.add('visually-hidden');
   }
+});
+priceRangeFilterBtnLeft.addEventListener('mouseup', function (evt) {
+  var value = getPriceRangePinCoordinate(evt.target);
+  priceRangeMinPinValue.textContent = value;
+});
+priceRangeFilterBtnRight.addEventListener('mouseup', function (evt) {
+  var value = getPriceRangePinCoordinate(evt.target);
+  priceRangeMaxPinValue.textContent = value;
 });
