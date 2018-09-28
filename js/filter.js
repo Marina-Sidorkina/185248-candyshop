76e84311f586876a98eedce3e7533e6f6d9dc085
Marina Sidorkina <m.sidorkina55@gmail.com>
2018-09-28 19:28:47 +0300

    Добавляет подвижность слайдера, задающего цену товара

--- js/filter.js
+++ js/filter.js
@@ -9,22 +9,69 @@
   var priceRangeFilter = document.querySelector('.range__filter');
   var priceRangeFilterCoordinate = priceRangeFilter.getBoundingClientRect();
   var priceRangeStart = priceRangeFilterCoordinate.x;
+  var priceRangeEnd = priceRangeFilterCoordinate.right;
   var priceRangeWidth = priceRangeFilterCoordinate.width;
   var priceRangeStep = priceRangeWidth / PRICE_RANGE;
+  var priceRangeFilterBtnShiftOne = priceRangeFilterBtnLeft.getBoundingClientRect().width / 10 * 4;
+  var priceRangeFilterBtnShiftTwo = priceRangeFilterBtnLeft.getBoundingClientRect().width / 5;
+  var fillLine = document.querySelector('.range__fill-line');
 
   var getPriceRangePinCoordinate = function (evtTarget) {
     var pinCoordinate = evtTarget.getBoundingClientRect();
-    var pinX = pinCoordinate.x;
+    var pinX = pinCoordinate.x + (pinCoordinate.width / 2);
     var pinValue = Math.round((pinX - priceRangeStart) / priceRangeStep);
     return pinValue;
   };
 
-  priceRangeFilterBtnLeft.addEventListener('mouseup', function (evt) {
-    var value = getPriceRangePinCoordinate(evt.target);
-    priceRangeMinPinValue.textContent = value;
+  var onPriceRangeFilterBtnMousedown = function (evt, evtTarget) {
+    var startCoord = evt.clientX;
+    var secondBtn = evt.target === priceRangeFilterBtnLeft ?
+      priceRangeFilterBtnRight : priceRangeFilterBtnLeft;
+    var secondBtnX = secondBtn.getBoundingClientRect().x;
+    var secondBtnRight = secondBtn.getBoundingClientRect().right;
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      if (
+        evt.target === priceRangeFilterBtnLeft
+      && moveEvt.clientX >= (priceRangeStart + priceRangeFilterBtnShiftOne)
+      && moveEvt.clientX < (secondBtnX + priceRangeFilterBtnShiftTwo)
+      ||
+      evt.target === priceRangeFilterBtnRight
+      && moveEvt.clientX <= (priceRangeEnd - priceRangeFilterBtnShiftOne)
+      && moveEvt.clientX > (secondBtnRight - priceRangeFilterBtnShiftTwo)) {
+        var shift = startCoord - moveEvt.clientX;
+        startCoord = moveEvt.clientX;
+        evtTarget.style.left = (evtTarget.offsetLeft - shift) + 'px';
+        fillLine.style.left = priceRangeFilterBtnLeft.offsetLeft + 'px';
+        fillLine.style.right = priceRangeFilterCoordinate.width - priceRangeFilterBtnRight.offsetLeft + 'px';
+        if (evt.target === priceRangeFilterBtnLeft) {
+          priceRangeMinPinValue.textContent = getPriceRangePinCoordinate(evt.target);
+        } else if (evt.target === priceRangeFilterBtnRight) {
+          priceRangeMaxPinValue.textContent = getPriceRangePinCoordinate(evt.target);
+        }
+      }
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      if (evt.target === priceRangeFilterBtnLeft) {
+        priceRangeMinPinValue.textContent = getPriceRangePinCoordinate(evt.target);
+      } else if (evt.target === priceRangeFilterBtnRight) {
+        priceRangeMaxPinValue.textContent = getPriceRangePinCoordinate(evt.target);
+      }
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+
+  priceRangeMinPinValue.textContent = getPriceRangePinCoordinate(priceRangeFilterBtnLeft);
+  priceRangeMaxPinValue.textContent = getPriceRangePinCoordinate(priceRangeFilterBtnRight);
+  priceRangeFilterBtnLeft.addEventListener('mousedown', function (evt) {
+    onPriceRangeFilterBtnMousedown(evt, evt.target);
   });
-  priceRangeFilterBtnRight.addEventListener('mouseup', function (evt) {
-    var value = getPriceRangePinCoordinate(evt.target);
-    priceRangeMaxPinValue.textContent = value;
+  priceRangeFilterBtnRight.addEventListener('mousedown', function (evt) {
+    onPriceRangeFilterBtnMousedown(evt, evt.target);
   });
 })();
